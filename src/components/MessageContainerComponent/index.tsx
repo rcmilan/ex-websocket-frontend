df0297574e3fcf56a6293f52a6fd3f05edf9ad30
Ricardo Silva Milan <13418519+rcmilan@users.noreply.github.com>
2021-11-07 17:04:10 -0300

    adicionando comentários

--- src/components/MessageContainerComponent/index.tsx
+++ src/components/MessageContainerComponent/index.tsx
@@ -6,22 +6,25 @@ interface Props {
 }
 
 function MessageContainerComponent({ messages }: Props) {
+  // Referência a div do componente
   const messageRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (messageRef && messageRef.current) {
       const { scrollHeight, clientHeight } = messageRef.current;
+      
+      // faz scroll da div até o final
       messageRef.current.scrollTo({
         left: 0,
         top: scrollHeight - clientHeight,
         behavior: "smooth",
       });
     }
-  }, [messages]);
+  }, [messages]); // sempre que houver alteração nas messages
 
   return (
     <div ref={messageRef} className="message-container">
-      {messages.map((m, index) => (
+      {messages.map((m, index) => ( // mensagens enviadas
         <div key={index} className="user-message">
           <div className="message bg-primary">{m.message}</div>
           <div className="from-user">{m.user}</div>
